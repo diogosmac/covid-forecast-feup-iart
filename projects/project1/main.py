f615181a113d0e296c61c6da944c40529fabee0c
Diogo Machado <diogo.smac@gmail.com>
2020-03-14 19:23:28 +0000

    changed name to main.py

---
+++ projects/project1/main.py
@@ -0,0 +1,15 @@
+from dataset import Dataset
+
+
+def main():
+    datasetA = Dataset('a_example')
+    datasetB = Dataset('b_should_be_easy')
+    datasetC = Dataset('c_no_hurry')
+    datasetD = Dataset('d_metropolis')
+    datasetE = Dataset('e_high_bonus')
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
